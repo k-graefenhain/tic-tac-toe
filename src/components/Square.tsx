0ae0c1ce09fafb6b1367223561e302dbe3c5c5e5
Katja Gräfenhain <katja@graefenhain.ch>
2023-05-01 10:34:40 +0200

    use state

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,17 +1,33 @@
 import React, {Component} from "react";
+import {useState} from "react";
 
 export interface SquareProps {
-    value: number
+    // value: number
 }
 
 export class Square extends Component<SquareProps, any> {
     constructor(props: SquareProps) {
         super(props);
+        this.state = {
+            value: null
+        }
     }
 
     render() {
         return (
-            <button className="square">{this.props.value}</button>
+            <button
+                className="square"
+                onClick={(e: React.MouseEvent) => {this.handleClick()}}
+            >
+                {this.state.value}
+            </button>
         );
+    }
+
+    handleClick() {
+        // console.log("clicked");
+        this.setState({
+            value: "x"
+        });
     }
 }
